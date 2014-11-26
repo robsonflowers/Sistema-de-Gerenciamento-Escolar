5de6b568fa6d9e56f7497a7b142a64a57c617ed4
Robson Flores <robsonflowers@gmail.com>
2014-11-25 22:10:01 -0200

    adicionada pagina pages/home.php e concluida a adaptacao do antigo arquivo files/css/estilo.css para o arquivo files/css/mainstyle.scss

---
+++ pages/home.php
@@ -0,0 +1,50 @@
+<div class="centralizar">
+    <div id="container">
+        <section id="boxes-opcoes">
+            <div class="box"><span>28</span>Alunos</div>
+            <div class="box"><span>6</span>Professores</div>
+            <div class="box"><span>4</span>Auxiliares</div>
+            <br>
+            <div class="box"><span>!</span>Financeiro</div>
+            <div class="box">Quadro de avisos</div>
+            <div class="box">Sistema</div>
+        </section>
+        <section id="quadro-aviso">
+            <div id="titulo">Quadro de avisos</div>
+            <article class="aviso">
+                <div id="titulo-aviso">Férias de verão</div>
+                <div id="data-aviso">
+                    <span id="dia">15</span>
+                    <div class="clear"></div>
+                    <span id="mes">Nov</span>
+                </div>
+                <div id="resumo-aviso">Programação de férias de verão 2015</div>
+            </article>
+            <article class="aviso">
+                <div id="titulo-aviso">Reunião de fechamento de mês</div>
+                <div id="data-aviso">
+                    <span id="dia">25</span>
+                    <div class="clear"></div>
+                    <span id="mes">Nov</span>
+                </div>
+                <div id="resumo-aviso">Programação de férias de verão 2015</div>
+            </article>
+            <article class="aviso">
+                <div id="titulo-aviso">Natal</div>
+                <div id="data-aviso">
+                    <span id="dia">28</span>
+                    <div class="clear"></div>
+                    <span id="mes">Nov</span>
+                </div>
+                <div id="resumo-aviso">Festa de final de ano com troca de lembranças</div>
+            </article>
+        </section>
+    </div>
+    <section id="calendario">
+        <?php include_once 'calendario.php'?>;
+    </section>
+
+
+    <div class="clear"></div>
+</div>
+            
